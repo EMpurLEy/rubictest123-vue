7d57cf60229d831bd5e31ff318aac067ea91e3f2
Tatyana-Essentya <dlyarubicona@gmail.com>
2021-10-03 15:38:17 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Books</h1>
+        <hr><br><br>
+        <button type="button" class="btn btn-success btn-sm">Добавить</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Название</th>
+              <th scope="col">Содержимое</th>
+              <th scope="col">Дата?</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(note, index) in notes" :key="index">
+            <tr>
+              <td>{(note.Title)}</td>
+              <td>{(note.Body)}</td>
+              <td>{(note.ChangeData)}</td>
+              <td>
+                <button type="button" class="btn btn-warning btn-sm">Update</button>
+                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      Notes: [],
+    };
+  },
+  methods: {
+    getNotes() {
+      const path = 'http://localhost:56858/api/Notes';
+      axios.get(path)
+        .then((res) => {
+          this.notes = res.data.data;
+        })
+        .catch((error) => {
+          // eslint-отключение следующей строки
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getNotes();
+  },
+};
+</script>
